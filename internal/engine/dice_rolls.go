632d0b38519a0dae394c42efa073b1229a84c151
Jaeiya <exanix@gmail.com>
2026-03-24 12:27:02 -0700

    fix(dice_rolls): relative ratio now pivots from `0.5`
    This is a fix because when primary > secondary, it would always return
    true, which is not a fair ratio. The ratio is now preserved no matter
    which stat is larger.

--- internal/engine/dice_rolls.go
+++ internal/engine/dice_rolls.go
@@ -7,13 +7,13 @@ import (
 // RollDodge checks if a character can evade an attack
 // based on the defenders dodge and attackers agility.
 func RollDodge(dodge, agility int) bool {
-	return relativeRatio(dodge, agility, 0.3)
+	return relativeRatio(dodge, agility)
 }
 
 // RollFirstAttack checks to see who strikes first in
 // combat based on the characters initiative.
 func RollFirstAttack(init1, init2 int) bool {
-	return relativeRatio(init1, init2, 0.5)
+	return relativeRatio(init1, init2)
 }
 
 // RollPercent returns true if pRNG returns a value within
@@ -23,7 +23,7 @@ func RollFirstAttack(init1, init2 int) bool {
 // chance is 10 then there is a 10% chance that a roll
 // will succeed.
 //
-// 🔴 Panics when chance < 0 or chance > 100
+// 🔴 Panics when chance < 0 or > 100
 func RollPercent(chance float64) bool {
 	if chance < 0 {
 		panic("cannot calculate a negative percent")
@@ -34,17 +34,47 @@ func RollPercent(chance float64) bool {
 	return rand.Float64() < (float64(chance) / 100.0)
 }
 
-func relativeRatio(stat1, stat2 int, weight float64) bool {
-	if stat2 == 0 && stat1 > 0 {
+// relativeRatio checks whether the primary stat has a chance
+// to succeed over the secondary stat.
+//
+// 🔵 The larger the primary stat, the higher the chance to
+// return true. The larger the secondary stat, the higher the
+// chance to return false.
+//
+// 🔵 If both stats are equal, the chance to succeed is 50%
+func relativeRatio(primary, secondary int) bool {
+	if primary < 0 || secondary < 0 {
+		panic("stats should never be less than zero")
+	}
+
+	var chance float64
+	f1, f2 := float64(primary), float64(secondary)
+	pivot := 0.5
+
+	switch {
+	case (f1 == 0 && f2 == 0) || f1 == f2:
+		chance = pivot
+
+	case f1 == 0:
+		return false
+
+	case f2 == 0:
+		return true
+
+	case f2 > f1:
+		chance = f1 / f2 * pivot
+
+	default:
+		chance = 1.0 - (f2 / f1 * pivot)
+	}
+
+	if chance > 0.99 {
 		return true
 	}
 
-	coefficient := float64(stat1) / float64(stat2)
-	// If both stats are equal, then the weight becomes the chance
-	chance := coefficient * weight
-
-	if chance >= 1.0 {
-		return true
+	if chance < 0.01 {
+		return false
 	}
+
 	return rand.Float64() < chance
 }
